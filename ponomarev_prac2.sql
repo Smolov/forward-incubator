56456c23216249cd7d8f214b76662c7c6bbcdbe5
Igor <42449842+Smolov@users.noreply.github.com>
2018-09-05 23:15:36 +0300

    Add files via upload

---
+++ ponomarev_prac2.sql
@@ -0,0 +1,49 @@
+REM   Script: practika2.3
+REM   prac2
+
+---1
+
+
+--Конфликт связан с изменением id_contract_inst во времени
+
+ cursor cur_con is 
+
+
+      select con.id_contract_inst 
+        from fw_contracts con 
+       where con.id_contract_inst != pID_CONTRACT_INST 
+         and con.v_ext_ident = l_ext_ident 
+--Нужно добавить именно эту строчку в курсор 
+and con.DT_START<=current_timestamp  and con.DT_STOP>current_timestamp 
+ 
+ 
+ 
+ 
+--2 
+ 
+--Необходимый файл (видимо случайно) удалили, поставив в B_DELETED=1 
+ 
+UPDATE filestorage 
+SET B_DELETED=0 
+WHERE V_SHORT_NAME='rpt_change.rpt' ;
+
+--3
+--Отсутствует таблица fw_service, т.е.
+INSERT INTO fw_service .... 
+ 
+ 
+ 
+ 
+---4 
+ 
+ 
+/* 
+Дело в том что срок действия идентификаторов услуг истек, а некоторые идентификаторы услуг и вовсе отсутствуют (170033,191817,174491,191849). 
+Решение: добавить пустые идентификаторы в базу, 
+а старые обновить.  
+P.S. или нет 
+*/ ;
+
+select * from cost 
+;
+
